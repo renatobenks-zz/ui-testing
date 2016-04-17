564b9f6dfa146ff254b68f43134ec2c7793e69f5
Renato Benkendorf <renato-benkendorf@hotmail.com>
2016-04-17 20:21:55 -0300

    build project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,18 @@
 /*global module*/
 module.exports = function (grunt) {
   'use strict'; 
-  var gruntConfig = {};
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  gruntConfig.uglify = {
-      options: {
-        mangle: false
-      },
-      my_target: {
-        files:{
-          'dest/script.min.js':['public_html/js/script.js', 'public_html/js/featureScroll.js'],
+    grunt.initConfig({
+      uglify: {
+        options: {
+          mangle: false
+        },
+        my_target: {
+          files:{
+            'dest/script.min.js':['public_html/js/script.js', 'public_html/js/featureScroll.js'],
+          }
         }
-      }
-  };
-  grunt.initConfig(gruntConfig);
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default', 'uglify');
 };
