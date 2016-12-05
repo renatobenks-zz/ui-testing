e0794711fc449e83e5ea53482b41647d07874ba5
Renato Benkendorf <renato-benkendorf@hotmail.com>
2016-12-04 22:45:45 -0200

    Solved styles

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var cookieParser = require('cookie-parser');
 
-var filesLoad = ['app', 'bower_components', 'build'];
+var filesLoad = ['app', 'bower_components'];
 for (var file in filesLoad) {
     app.use(express.static(filesLoad[file]));
 }
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
     res.render(__dirname + '/app/');
 });
 
-var server = app.listen(9000, function () {
+var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log('App listening at http://localhost:' + port);
 });
